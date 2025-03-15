ff8b7e05caf2a64133e0342b0e53a3aa527a4d15
Khong <hankhongg@gmail.com>
2025-03-15 17:32:47 +0700

    important: how to make an interface be a crud repository

--- src/main/java/com/hankhongg/postgresql/repositories/AuthorRepository.java
+++ src/main/java/com/hankhongg/postgresql/repositories/AuthorRepository.java
@@ -1,4 +1,9 @@
 package com.hankhongg.postgresql.repositories;
 
-public interface AuthorRepository {
+import com.hankhongg.postgresql.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
